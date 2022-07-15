98ad3236b61b2417eda9963f89f556077f99730d
Vasko Nikolov <hugovasko1@gmail.com>
2022-07-15 16:04:35 +0300

    Final result
    It become copy cat code at the end...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
-import useFetch from "./hooks/use-fetch";
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const fetchData = useFetch();
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-movies-fa5ce-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
   const taskAddHandler = (task) => {
-    fetchData.setTasks((prevTasks) => prevTasks.concat(task));
+    setTasks((prevTasks) => prevTasks.concat(task));
   };
 
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
       <Tasks
-        items={fetchData.tasks}
-        loading={fetchData.isLoading}
-        error={fetchData.error}
-        onFetch={fetchData}
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
       />
     </React.Fragment>
   );
 }
+
 export default App;
